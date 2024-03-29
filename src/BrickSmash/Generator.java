502d92ce77832e9cae7a90e394b35a4d82ff8e02
akashyerra <akashyerra@gmail.com>
2024-03-29 19:32:06 +0530

    Built brick smasher game

---
+++ src/BrickSmash/Generator.java
@@ -0,0 +1,43 @@
+package BrickSmash;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Graphics2D;
+
+public class Generator {
+    public int way[][];
+    public int brickWidth;
+    public int brickHeight;
+    
+    public Generator(int row, int column){
+        way = new int [row][column];
+        for(int i=0; i<way.length; i++){
+            for(int j=0; j<way[0].length; j++){
+                way[i][j] = 1;
+            }
+        }
+
+        brickWidth = 540/column;
+        brickHeight = 150/row;
+    }
+
+    public void draw(Graphics2D g){
+        for(int i=0; i<way.length; i++){
+            for(int j=0; j<way[0].length; j++){
+                if(way[i][j] > 0){
+                    g.setColor(Color.white);
+                    g.fillRect(j*brickWidth + 80, i*brickHeight + 50, brickWidth, brickHeight);
+
+                    g.setStroke(new BasicStroke(3));
+                    g.setColor(Color.black);
+                    g.drawRect(j*brickWidth + 80, i*brickHeight + 50, brickWidth, brickHeight);
+                }
+            }
+        }
+    }
+
+    public void setBrickValue(int value, int row, int column){
+        way[row][column] = value;
+    }
+
+}
